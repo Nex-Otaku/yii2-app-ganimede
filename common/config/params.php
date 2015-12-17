20ed46dfbfe95228104cec2bb202dbf9a83c87f7
Nex Otaku <nex@otaku.ru>
2015-12-17 14:25:32 +0300

    Добавил глобальные параметры по умолчанию.

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,13 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    // STUB поменять перед релизом адрес почтового робота.
+    'robotEmail' => 'robot@myhost.ru',
     'user.passwordResetTokenExpire' => 3600,
+    // STUB перенести этот параметр в настройки файл-менеджера по умолчанию.
+    'lib' => '/upload/',
+    'site_title' => 'My Cool Site',
+    'site_title_for_tab' => 'My Cool Site',
+    'aliases' => [
+            '@main_root' => realpath(dirname(__FILE__) . '/../../'),
+        ],
 ];

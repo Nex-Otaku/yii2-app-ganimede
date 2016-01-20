5e89e1c5b9b618a70131a00c6f109c20361127ad
Nex Otaku <nex@otaku.ru>
2016-01-20 17:05:17 +0300

    Настроил отладочную панель.

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -14,6 +14,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*'],
     ];
 
     $config['bootstrap'][] = 'gii';

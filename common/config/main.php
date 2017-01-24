7803347da629a3219e76f84c5218edbe23267487
Nex Otaku <nex@otaku.ru>
2017-01-24 09:16:00 +0300

    Добавил локализацию.

--- common/config/main.php
+++ common/config/main.php
@@ -15,6 +15,22 @@ return [
             'decimalSeparator' => ',',
             'thousandSeparator' => ' ',
         ],
+        // Все используемые текстовые сообщения
+        // выводятся через переводчик.
+        // Это позволяет быстро перевести сгенерированные скрипты.
+        // Также, это позволит в будущем 
+        // делать проекты для иностранных заказчиков.
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'fileMap' => [
+                        'app' => 'app.php'
+                    ]
+                ],
+            ],
+        ],
     ],
     'aliases' => [
         '@main_root' => realpath(dirname(__FILE__) . '/../../'),
